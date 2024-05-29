e02615ae65aa209a996c4a20e4ad3ac15a285b30
dnieto1997 <121445107+dnieto1997@users.noreply.github.com>
2024-05-29 15:47:41 -0500

    act

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,6 @@ async function bootstrap() {
   app.use(urlencoded({ extended: true, limit: '100mb' }));
   app.enableCors();
 
-  await app.listen(process.env.PORT);
-  /*   await app.listen(3000); */
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
